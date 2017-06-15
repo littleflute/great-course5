2f804f456ca5390c95888d68b11c01cadbc96087
littleflute <13699175164@sina.cn>
2017-06-15 16:08:54 -0500

    Add files via upload

---
+++ cd3/m.js
@@ -0,0 +1,14 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 5-1 Book of Exodus.mp3";		
+f[2]	= "02 5-2 Book of Exodus.mp3";		
+f[3]	= "03 5-3 Book of Exodus.mp3";		
+f[4]	= "04 5-4 Book of Exodus.mp3";		
+f[5]	= "05 5-5 Book of Exodus.mp3";		
+f[6]	= "06 5-6 Book of Exodus.mp3";		
+f[7]	= "07 6-1 Gospel of Mark.mp3";		
+f[8]	= "08 6-2 Gospel of Mark.mp3";		
+f[9]	= "09 6-3 Gospel of Mark.mp3";		
+f[10]	= "10 6-4 Gospel of Mark.mp3";		
+f[11]	= "11 6-5 Gospel of Mark.mp3";		
+f[12]	= "12 6-6 Gospel of Mark.mp3";		
